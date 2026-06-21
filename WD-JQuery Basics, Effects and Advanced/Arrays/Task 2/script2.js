53306fb36d14827418e8b919bbba1979e3f71981
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 08:32:12 +0530

    Update script2.js

--- WD-JQuery Basics, Effects and Advanced/Arrays/Task 2/script2.js
+++ WD-JQuery Basics, Effects and Advanced/Arrays/Task 2/script2.js
@@ -1,25 +1,15 @@
 function findSum() {
 
-    // Get input value
     let input = document.getElementById("numbers").value;
 
-    // Convert string into array
     let arr = input.split(",");
 
-    // Store total sum
     let sum = 0;
 
-    // Loop through array
     for (let i = 0; i < arr.length; i++) {
-
-        sum = sum + Number(arr[i]);
-
+        sum += Number(arr[i]);
     }
 
-    // Show Result
     document.getElementById("result").innerHTML =
         "Sum = " + sum;
-    console.log("Input: " + input);
-    console.log("Output: " + sum);
 }
-findSum();
