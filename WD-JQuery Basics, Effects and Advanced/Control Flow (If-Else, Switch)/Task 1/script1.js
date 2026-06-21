fc9e2c34c60f9fe25ed25cdf4c3009fd75c546d4
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 08:45:49 +0530

    Update script1.js

--- WD-JQuery Basics, Effects and Advanced/Control Flow (If-Else, Switch)/Task 1/script1.js
+++ WD-JQuery Basics, Effects and Advanced/Control Flow (If-Else, Switch)/Task 1/script1.js
@@ -1,60 +1,46 @@
 function checkNumber() {
 
-    // Get Input
-    const input =
-        document.getElementById("userInput").value;
+    const input = document.getElementById("userInput").value;
 
-    // Result Element
-    const display =
-        document.getElementById("result");
+    const display = document.getElementById("result");
 
-    // Card Element
-    const card =
-        document.getElementById("mainCard");
+    const card = document.getElementById("mainCard");
 
-    // Split Values
     const numbers = input.split(",");
 
-    // Result Variable
     let result = "";
 
-    // Remove Old Borders
     card.classList.remove(
         "positive-border",
         "negative-border"
     );
 
-    // Loop Through Numbers
     for (let i = 0; i < numbers.length; i++) {
 
-        let num = parseInt(numbers[i]);
+        let num = parseInt(numbers[i].trim());
 
-        // Positive Number
         if (num > 0) {
 
-            result +=
-                num + " is Positive Integer <br>";
+            result += num + " is Positive Integer<br>";
 
             card.classList.add("positive-border");
+
         }
 
-        // Negative Number
         else if (num < 0) {
 
-            result +=
-                num + " is Negative Integer <br>";
+            result += num + " is Negative Integer<br>";
 
             card.classList.add("negative-border");
+
         }
 
-        // Zero
         else {
 
-            result +=
-                num + " is Zero <br>";
+            result += num + " is Zero<br>";
+
         }
     }
 
-    // Display Result
     display.innerHTML = result;
 }
