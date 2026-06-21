831ad011771e7de289a4ab5e54dff3cb617f80d6
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 09:27:11 +0530

    Update script2.js

--- WD-JQuery Basics, Effects and Advanced/Functions/Task 2/script2.js
+++ WD-JQuery Basics, Effects and Advanced/Functions/Task 2/script2.js
@@ -1,54 +1,11 @@
-// Function to calculate sum
-function calculateSum(a, b){
+function calculateSum() {
 
-    return a + b;
+    let num1 = Number(document.getElementById("num1").value);
 
-}
+    let num2 = Number(document.getElementById("num2").value);
 
-// Button Function
-function handleCalculation(){
+    let sum = num1 + num2;
 
-    // Input Values
-    const num1 =
-        parseFloat(
-            document.getElementById(
-                "num1"
-            ).value
-        );
-
-    const num2 =
-        parseFloat(
-            document.getElementById(
-                "num2"
-            ).value
-        );
-
-    // Result Box
-    const resultBox =
-        document.getElementById(
-            "result"
-        );
-
-    // Validation
-    if(isNaN(num1) || isNaN(num2)){
-
-        resultBox.className =
-            "alert alert mt-5 text-center";
-
-        resultBox.innerHTML =
-            "Please enter valid numbers";
-
-        return;
-    }
-
-    // Calculate
-    const sum =
-        calculateSum(num1, num2);
-
-    // Show Result
-    resultBox.className =
-        "alert alert-success mt-5 text-center";
-
-    resultBox.innerHTML =`Result: +{sum}`;
-
+    document.getElementById("result").innerHTML =
+        "Result: " + sum;
 }
