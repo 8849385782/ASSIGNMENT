3f252129010c85c3a39a7e0514ad0f2269fbdc17
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 09:51:35 +0530

    Update script1.js

--- WD-JQuery Basics, Effects and Advanced/JavaScript Timing Events (setTimeout, setInterval)/Task1/script1.js
+++ WD-JQuery Basics, Effects and Advanced/JavaScript Timing Events (setTimeout, setInterval)/Task1/script1.js
@@ -4,10 +4,12 @@ setTimeout(function() {
 
     alert("5 seconds completed");
 
+    const heading = document.getElementById("heading");
+
+    heading.textContent = "Color Changed!";
+
+    heading.style.color = "white";
+
     document.body.style.backgroundColor = "blue";
-
-    document.getElementById("heading").innerHTML = "Color Changed!";
-
-    document.getElementById("heading").style.color = "white";
 
 }, 5000);
