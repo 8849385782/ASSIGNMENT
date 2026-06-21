54ef2ac65a571faa95a237b55126e97c3b1d5c80
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 08:49:10 +0530

    Update script1.js

--- WD-JQuery Basics, Effects and Advanced/Control Flow (If-Else, Switch)/Task 1/script1.js
+++ WD-JQuery Basics, Effects and Advanced/Control Flow (If-Else, Switch)/Task 1/script1.js
@@ -1,44 +1,38 @@
 function checkNumber() {
 
-    const input = document.getElementById("userInput").value;
+    const input = document.getElementById("userInput").value.trim();
 
     const display = document.getElementById("result");
 
     const card = document.getElementById("mainCard");
 
+    if (input === "") {
+        display.innerHTML = "Please enter numbers";
+        return;
+    }
+
     const numbers = input.split(",");
 
     let result = "";
 
-    card.classList.remove(
-        "positive-border",
-        "negative-border"
-    );
+    card.classList.remove("positive-border", "negative-border");
 
     for (let i = 0; i < numbers.length; i++) {
 
-        let num = parseInt(numbers[i].trim());
+        let num = Number(numbers[i].trim());
 
         if (num > 0) {
-
             result += num + " is Positive Integer<br>";
-
             card.classList.add("positive-border");
-
         }
 
         else if (num < 0) {
-
             result += num + " is Negative Integer<br>";
-
             card.classList.add("negative-border");
-
         }
 
         else {
-
             result += num + " is Zero<br>";
-
         }
     }
 
