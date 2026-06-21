3042498162786193abb596c1637b71713cee04fd
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 10:14:55 +0530

    Update script1.js

--- WD-JQuery Basics, Effects and Advanced/Loops (For, While, Do-While)/Task1/script1.js
+++ WD-JQuery Basics, Effects and Advanced/Loops (For, While, Do-While)/Task1/script1.js
@@ -1,21 +1,7 @@
-// Target div
 const display = document.getElementById("number-list");
 
-// Loop from 1 to 10
-for(let i = 1; i <= 10; i++){
+for(let i = 1; i <= 10; i++) {
 
-    // Create div
-    const numberBox = document.createElement("div");
-
-    // Add class
-    numberBox.className = "number-box";
-
-    // Add number
-    numberBox.textContent = i;
-
-    // Append to page
-    display.appendChild(numberBox);
-
-    // Console
-    console.log(i);
+    display.innerHTML +=
+        "<div class='number-box'>" + i + "</div>";
 }
