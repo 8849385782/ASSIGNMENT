233a08139ca88babc3bcea56f0dd8b196532aa63
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-06-21 09:47:45 +0530

    Update script1.js

--- WD-JQuery Basics, Effects and Advanced/JavaScript Timing Events (setTimeout, setInterval)/Task1/script1.js
+++ WD-JQuery Basics, Effects and Advanced/JavaScript Timing Events (setTimeout, setInterval)/Task1/script1.js
@@ -1,14 +1,10 @@
-// Wait for 5 seconds
 setTimeout(() => {
 
-    // Change Bootstrap background class
     document.body.classList.remove("bg-light");
     document.body.classList.add("bg-primary");
 
-    // Change text
     document.getElementById("heading").innerText = "Color Changed!";
 
-    // Change text color
     document.getElementById("heading").classList.remove("text-dark");
     document.getElementById("heading").classList.add("text-white");
 
