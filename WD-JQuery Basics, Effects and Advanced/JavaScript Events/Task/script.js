e90ab9761268562a1bd5f210b5992b85abf7e10e
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-07-10 00:49:11 +0530

    Delete WD-JQuery Basics, Effects and Advanced/JavaScript Events directory

--- WD-JQuery Basics, Effects and Advanced/JavaScript Events/Task/script.js
+++
@@ -1,7 +0,0 @@
-// Select the button element by its ID
-const button = document.getElementById('myButton');
-
-// Add the event listener
-button.addEventListener('click', function() {
-    alert('Button clicked!');
-});
