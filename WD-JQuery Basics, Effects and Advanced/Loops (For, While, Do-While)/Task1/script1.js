f343d1c2587218be752392b5ae596808f41547e3
Daya trivedi <139197025+8849385782@users.noreply.github.com>
2026-07-10 00:49:44 +0530

    Delete WD-JQuery Basics, Effects and Advanced/Loops (For, While, Do-While) directory

--- WD-JQuery Basics, Effects and Advanced/Loops (For, While, Do-While)/Task1/script1.js
+++
@@ -1,7 +0,0 @@
-const display = document.getElementById("number-list");
-
-for(let i = 1; i <= 10; i++) {
-
-    display.innerHTML +=
-        "<div class='number-box'>" + i + "</div>";
-}
--- WD-JQuery Basics, Effects and Advanced/Loops (For, While, Do-While)/Task3/script result.js
+++
@@ -1,9 +0,0 @@
-let number;
-
-do {
-    // Prompt always returns a string; parseFloat converts it to a number
-    let input = prompt("Enter a number greater than 10:");
-    number = parseFloat(input);
-} while (isNaN(number) || number <= 10);
-
-alert(`Thank you! Loop finished. You entered: ${number}`);
